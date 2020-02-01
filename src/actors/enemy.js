c7edfeeaf924fcee394bf76102e79d7152c344c5
AdamNellis <adam.nellis@simomics.com>
2020-02-01 22:55:49 +0000

    Added enemy that flies into centre then shoots player

--- src/actors/enemy.js
+++ src/actors/enemy.js
@@ -17,12 +17,21 @@ export default class Enemy extends CircularCollider {
 		this.rotation_force = 0;
 		this.angular_rotation = 0;
 		this.physics_angle = rotation_angle;
+
+		this.force_x = 0;
+		this.force_y = 0;
+		this.velocity_x = 0;
+		this.velocity_y = 0;
 	}
 
 	update(t, dt) {
 		// Integrate motion
 		this.physics_angle += this.angular_rotation * dt;
+		this.x += this.velocity_x * dt;
+		this.y += this.velocity_y * dt;
 		this.angular_rotation += this.rotation_force * dt;
+		this.velocity_x += this.force_x * dt;
+		this.velocity_y += this.force_y * dt;
 		// Set rotation on shape
 		this.rotation = this.physics_angle + this.rotation_angle;
 	}
@@ -39,7 +48,6 @@ export default class Enemy extends CircularCollider {
 
 	shoot() {
 		// Create a bullet moving in the direction that the enemy is pointing
-		// TODO: Take into account this.rotation_angle
 		this.bullets.add(new Bullet(this.scene, this.x, this.y, -Math.sin(this.rotation), Math.cos(this.rotation)))
 	}
 }
