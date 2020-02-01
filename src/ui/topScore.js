0d1fde58095bd7fdadba6e6dccca37efe2abfc2c
Jacob.Busfield <Jacob.Busfield@simomics.com>
2020-02-01 23:07:07 +0000

    Top score added

---
+++ src/ui/topScore.js
@@ -0,0 +1,25 @@
+export default class TopScore extends Phaser.GameObjects.Text {	
+	constructor(scene, score) {
+        super(scene, 900, 580,
+            "Best: 0" + score, {
+                font: "bold 48px Arial",
+                fill: '#fff',
+                align: 'center',
+                wordWrap: {
+                    width: 400,
+                    useAdvancedWrap: true
+                }
+            }).setOrigin(0.5, 0)
+            
+        let best = JSON.parse(localStorage.getItem('SM_GAME_BEST'))
+        
+        if (!best || (score > best)){
+            localStorage.setItem('SM_GAME_BEST', JSON.stringify(score))
+            this.setText("Best: " + score)
+        } else {
+            this.setText("Best: " + best)
+        }
+			
+        scene.add.existing(this)
+	}
+}
