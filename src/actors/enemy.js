2731e591799aefc803e30a5ad4574a634c2e40b5
AdamNellis <adam.nellis@simomics.com>
2020-02-01 13:20:42 +0000

    Enemies now point in the correct direction

--- src/actors/enemy.js
+++ src/actors/enemy.js
@@ -5,6 +5,7 @@ import {
 export default class Enemy extends GameObjects.Sprite {
 	constructor(scene, player, x, y) {
 		super(scene, x, y, "spaceRedux", "playerShip1_blue.png")
+		this.angle = Math.PI / 2;
 
 		// this.setOrigin(0, 0);
 		this.player = player;
@@ -14,17 +15,20 @@ export default class Enemy extends GameObjects.Sprite {
 
 		this.rotation_force = 0;
 		this.angular_rotation = 0;
+		this.rotation_angle = Math.PI / 2; // rotation, assuming 0 is up the y axis (because that's how the image is on the sprite sheet)
 	}
 
 	update(t, dt) {
 		// Integrate motion
-		this.rotation += this.angular_rotation * dt;
+		this.rotation_angle += this.angular_rotation * dt;
 		this.angular_rotation += this.rotation_force * dt;
+		// Set rotation on shape
+		this.rotation = this.rotation_angle + Math.PI / 2;
 	}
 
 	update_delayed(t, dt) {
 		// Calculate force
-		const angle_difference = normalise_angle_plus_minus_pi(Math.atan2(this.player.y - this.y, this.player.x - this.x) - this.rotation);
+		const angle_difference = normalise_angle_plus_minus_pi(Math.atan2(this.player.getYPosition() - this.y, this.player.getXPosition() - this.x) - this.rotation_angle);
 		const steering_force = this.rotation_rate * angle_difference;
 		const damping_force = this.angular_rotation * this.rotation_damping;
 		this.rotation_force = steering_force - damping_force;
