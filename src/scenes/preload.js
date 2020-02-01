5f0e9e24e5c7475b8b1ff73d4f367b189157a88b
Jacob.Busfield <Jacob.Busfield@simomics.com>
2020-02-01 16:01:19 +0000

    Crew picture

--- src/scenes/preload.js
+++ src/scenes/preload.js
@@ -11,6 +11,8 @@ export default class Preload extends Scene {
         this.load.image('body', require('../assets/body.png'));
         this.load.image('enemy', require('../assets/enemy.png'));
 
+        this.load.image('crew', require('../assets/crew.png'));
+
         this.load.atlasXML("spaceRedux", require("../assets/spaceRedux.png"), require("../assets/spaceRedux.xml"))
         this.load.atlasXML("spaceExtra", require("../assets/spaceExtra.png"), require("../assets/spaceExtra.xml"))
     }
