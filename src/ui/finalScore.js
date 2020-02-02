e99d57568538ee00963039b2bce662f79bf89ac0
Aiste Kiskyte <aiste.kiskyte@gmail.com>
2020-02-02 16:11:40 +0000

    dsadsad

--- src/ui/finalScore.js
+++ src/ui/finalScore.js
@@ -2,7 +2,7 @@ export default class FinalScore extends Phaser.GameObjects.Text {
 	constructor(scene, score) {
         super(scene, 750, 310,
             "Score: " + score, {
-                font: "bold 64px Arial",
+                font: "bold 40px Arial",
                 fill: '#0f0',
                 align: 'center',
                 wordWrap: {
